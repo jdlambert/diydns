7765fe7aeaa1c95dfa259ed9265838674263393a
Jacob Lambert <jacob.lambert@gmail.com>
2020-01-29 22:30:57 -0500

    Refactor main into subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use diydns::BytePacketBuffer;
-use std::env::args;
+use std::env;
 
-fn main() {
-    let args: Vec<String> = args().collect();
-    let mut buffer = BytePacketBuffer::from_file(args.get(1).unwrap()).unwrap();
+fn decode(args: Vec<String>) {
+    let mut buffer = BytePacketBuffer::from_file(args.get(2).unwrap()).unwrap();
 
     let packet = buffer.read_packet().unwrap();
     println!("{:#?}", packet.header);
@@ -19,5 +18,17 @@ fn main() {
     }
     for rec in packet.resources {
         println!("{:#?}", rec);
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    match args[1].as_str() {
+        "decode" => decode(args),
+        _ => {
+            println!("Unknown subcommand! Acceptable inputs: decode, resolve");
+            return;
+        }
     }
 }
