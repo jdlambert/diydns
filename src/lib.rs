17aea756305e3f96aa549b4fe35b1b8f20b98b1c
Jacob Lambert <jacob.lambert@gmail.com>
2020-01-29 20:52:09 -0500

    Add QueryType

--- src/lib.rs
+++ src/lib.rs
@@ -178,3 +178,25 @@ impl BytePacketBuffer {
         })
     }
 }
+
+#[derive(PartialEq, Eq, Debug, Clone, Hash, Copy)]
+pub enum QueryType {
+    Unknown(u16),
+    A,
+}
+
+impl QueryType {
+    pub fn to_num(&self) -> u16 {
+        match *self {
+            QueryType::Unknown(x) => x,
+            QueryType::A => 1,
+        }
+    }
+
+    pub fn from_num(num: u16) -> QueryType {
+        match num {
+            1 => QueryType::A,
+            _ => QueryType::Unknown(num),
+        }
+    }
+}
